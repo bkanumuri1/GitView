f373fd048e927de1f39dc55d23115da1f271732f
Anushka Suraj Kanal <akanal1@asu.edu>
2023-02-10 23:30:57 -0700

    US1 TS 45 sidebar for grading tool

---
+++ github-grading-tool-frontend-app/src/pages/AboutUs.js
@@ -0,0 +1,25 @@
+import React from "react";
+ 
+export const AboutUs = () => {
+  return (
+    <div className="home">
+      <h1>Git Repo</h1>
+    </div>
+  );
+};
+ 
+export const OurAim = () => {
+  return (
+    <div className="home">
+      <h1>Repo 1</h1>
+    </div>
+  );
+};
+ 
+export const OurVision = () => {
+  return (
+    <div className="home">
+      <h1>Repo 2</h1>
+    </div>
+  );
+};
