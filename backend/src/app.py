1dd67d0cb231a9fcd51c724b6268c2e1e0ed5216
Haritej Lakshmi Narayana <hlakshm2@asu.edu>
2023-02-14 23:20:14 -0700

    GitHub Oauth login implemented.Front-end and back-end(Proxy) are integrated.  User repos are fetched.

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,31 +1,56 @@
+import secrets
+from flask import Flask,request,jsonify, session
 import requests
+from flask_cors import CORS
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+CORS(app)
+app.secret_key = secrets.token_hex(16)
+CLIENT_ID = "e7231ef0e449bce7d695"
+CLIENT_SECRET = "d8c966df9903e6e9fe3a3372708daea192bdd041"
 
-def get_contributor_stats(repo_link, access_token):
-    headers = {
-        "Authorization": "Token " + access_token
+@app.route('/getAccessToken', methods=['GET'])
+def getAccessToken():
+    args = request.args
+    code = args.get("code")
+    url = "https://github.com/login/oauth/access_token"
+    headers = {'Accept':'application/json'}
+    data = {
+        'client_id':CLIENT_ID,
+        'client_secret':CLIENT_SECRET,
+        'code':code
     }
-    contributors_url = repo_link + "/collaborators"
-    contributors = requests.get(contributors_url, headers=headers).json()
-    for contributor in contributors:
-        username = contributor["login"]
-        commits_url = repo_link + "/commits?author=" + username
-        commits = requests.get(commits_url, headers=headers).json()
-        num_commits = len(commits)
-        print(f"Username: {username}, Number of commits: {num_commits}")
-        
+    response = requests.post(url,headers=headers,data=data)
+    # TODO:  validate response and send appropriate results
+    session['access_token']=response.json()['access_token']
+    print(session['access_token'])
+    return response.json()
 
-        pull_requests_url = repo_link + "/pulls?author=" + username
-        pull_requests = requests.get(pull_requests_url, headers=headers).json()
-        num_pull_requests = len(pull_requests)
-        print(f"Username: {username}, Number of Pull Requests: {num_pull_requests}")
+@app.route('/getUserData', methods=['GET'])
+def getUserData():
+    token = request.headers.get('Authorization')
+    url = "https://api.github.com/user"
+    headers = {'Authorization' : token}
+    response = requests.get(url,headers=headers)
+    session['username']=response.json()['login']
+    return response.json()
 
-        code_reviews = 0
-        for pull_request in pull_requests:
-            comments_url = pull_request["comments_url"]
-            comments = requests.get(comments_url, headers=headers).json()
-            code_reviews += len(comments)
-        print(f"Username: {username}, Number of code reviews: {code_reviews}")
+@app.route('/getUserRepos', methods=['GET'])
+def getUserRepos():
+    token = request.headers.get('Authorization')
+    url = "https://api.github.com/user/repos"
+    headers = {'Authorization' : token}
+    response = requests.get(url,headers=headers)
+    repoData = {}
+    for repository in response.json():
+        repoData[repository['id']] = repository['full_name']
+    return jsonify(repoData)
 
-repo_link = "https://api.github.com/repos/hlakshm2/CSE564-Assignment04"
-access_token = "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"
-get_contributor_stats(repo_link, access_token)
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=9000, debug=True)
+# [END gae_python3_app]
+# [END gae_python38_app]
