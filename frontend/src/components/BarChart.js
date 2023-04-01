4fdfc2ac4a7df2485fce31001e3925c40c546079
Bhavana Priya <63414566+bkanumuri1@users.noreply.github.com>
2023-03-31 20:54:00 -0700

    US138T154Add a pie chart

--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Bar,Line } from "react-chartjs-2";
+import { Bar,Line, Pie } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 function BarChart({ chartData }) {
   return <div style={{ position: "relative", margin: "auto", width: "80vw", height: '40vh' }}> <Bar data={chartData}   />
   <Line data={chartData}/>
-  
+  <Pie data = {chartData}/>
   
   </div>;
 }
--- frontend/src/components/Charts.js
+++ frontend/src/components/Charts.js
@@ -6,40 +6,6 @@ import BarChart from "./BarChart";
 
 const Chart = (props) => {
     const {commitData}=props;
-
-    
-    //  const UserData = [
-    //     {
-    //       id: 1,
-    //       year: 2016,
-    //       userGain: 80000,
-    //       userLost: 823,
-    //     },
-    //     {
-    //       id: 2,
-    //       year: 2017,
-    //       userGain: 45677,
-    //       userLost: 345,
-    //     },
-    //     {
-    //       id: 3,
-    //       year: 2018,
-    //       userGain: 78888,
-    //       userLost: 555,
-    //     },
-    //     {
-    //       id: 4,
-    //       year: 2019,
-    //       userGain: 90000,
-    //       userLost: 4555,
-    //     },
-    //     {
-    //       id: 5,
-    //       year: 2020,
-    //       userGain: 4300,
-    //       userLost: 234,
-    //     },
-    //   ];
       const [userData, setUserData] = useState({
         labels: commitData.map((data) => data.date),
         datasets: [
@@ -55,6 +21,16 @@ const Chart = (props) => {
           },
         ],
       });
+      // const [stats, setStats] = useState({
+      //   labels : commitData.map((data) => data.date), 
+      //   datasets:[
+      //     {
+      //     label: "Stats", 
+      //     data: commitData.map((data) =>data.commit_details.stats.total)
+      //     }
+      //   ]
+      // })
+
       useEffect(()=>{
         setUserData({labels: commitData.map((data) => data.date),
         datasets: [
