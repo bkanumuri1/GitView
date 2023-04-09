fece8fe561e560d700a00c69d464238b5ddaed9b
rahulvuppla <79802988+rahulvuppla@users.noreply.github.com>
2023-04-09 13:08:16 -0700

    US111T135 added pr charts

--- frontend/src/components/BarChart.js
+++ frontend/src/components/BarChart.js
@@ -4,10 +4,18 @@ import { Doughnut, Pie } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 function BarChart({ chartData }) {
-  return <div style={{ position: "relative", margin: "auto", width: "80vw", height: '40vh' }}>
-    <Bar data={chartData}/>
-    <Line data={chartData}/>
-  </div>;
+  return (
+    <div
+      style={{
+        position: "relative",
+        margin: "auto",
+        width: "80vw",
+        height: "40vh",
+      }}
+    >
+      <Bar data={chartData} />
+    </div>
+  );
 }
 
 export default BarChart;
--- frontend/src/components/PrCharts.js
+++ frontend/src/components/PrCharts.js
@@ -6,38 +6,6 @@ import BarChart from "./BarChart";
 const Charts = (props) => {
   const { prData } = props;
 
-  //  const UserData = [
-  //     {
-  //       id: 1,
-  //       year: 2016,
-  //       userGain: 80000,
-  //       userLost: 823,
-  //     },
-  //     {
-  //       id: 2,
-  //       year: 2017,
-  //       userGain: 45677,
-  //       userLost: 345,
-  //     },
-  //     {
-  //       id: 3,
-  //       year: 2018,
-  //       userGain: 78888,
-  //       userLost: 555,
-  //     },
-  //     {
-  //       id: 4,
-  //       year: 2019,
-  //       userGain: 90000,
-  //       userLost: 4555,
-  //     },
-  //     {
-  //       id: 5,
-  //       year: 2020,
-  //       userGain: 4300,
-  //       userLost: 234,
-  //     },
-  //   ];
   const [userData, setUserData] = useState({
     labels: prData.map((data) => data.date),
     datasets: [
